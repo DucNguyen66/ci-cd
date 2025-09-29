56d016cfc6d95486445c4af4322ded786db9a591
QuangDuc <ducbunny08@gmail.com>
2025-09-29 08:46:11 +0000

    Update

--- index.js
+++ index.js
@@ -9,3 +9,14 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
+	const express = require('express');
+	const app = express();
+	const port = 3000;
+
+	app.get('/', (req, res) => {
+		res.send('Hello CI/CD with Docker!');
+	});
+	
+	app.listen(port, () => {
+	 console.log('App is running on port ${port}');
+	});
