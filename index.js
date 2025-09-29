86fc67944729066910f18b3e114df26dedc940e2
DucNguyen66 <ducbunny08@gmail.com>
2025-09-29 15:31:09 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello CI/CD!');
+  res.send('Hello World!');
 });
 
-app.listen(PORT, '0.0.0.0', () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
